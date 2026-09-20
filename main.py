cb3defc7f07d1dd894c172bf25763c0807cf5a2b
Adam <ghazaliadam01@gmail.com>
2026-09-20 16:25:50 +0800

    Menambahkan print()

--- main.py
+++ main.py
@@ -1,1 +1,76 @@
-print("Hello, World!")
+nama = "Adam"
+
+
+print(nama)
+
+nama_panjang = "Adam Ahmad Gazhali"
+
+print(nama_panjang)
+
+angka_int1 = 10
+angka_int2 = 12
+
+print(angka_int1 + angka_int2)
+
+a = 10
+b = 12
+c = a + b
+print(c)
+
+print('''Halo saya Adam
+saya suka belajar coding
+Hari ini saya belajar bahasa pemrograman python''')
+
+teks = "halo saya adam"
+
+print(teks[2])
+print(teks[3])
+
+Hujan = False
+panas = True
+
+print(hujan)
+
+data = ("Adam", 10, True)
+
+print(data[0])
+print(data[1])
+
+data_mahasiswa = ("Rafi","034",True, 3.5)
+print(data_mahasiswa[0])
+print(data_mahasiswa[2])
+
+contoh_set = (1,2,3,4,5,6)
+print(contoh_set)
+
+mahasiswa = (
+    ("1234", "Rafi", 3.5),
+    ("5678", "Budi", 3.8),
+    ("andi", "9101", 3.9)
+)
+
+# a = mahasiswa[0][1]  # Mengakses nama dari tuple pertama
+# print(a)
+
+angka = 10
+print(type(angka))
+
+buku = {
+'judul' : 'Atomic Habits',
+'penulis' : 'James Clear',
+'halaman' : 320
+}
+...
+# Mengakses Value dalam Dictionary
+print(buku['judul'])
+print(buku['penulis'])
+print(buku['halaman'])
+
+a = True
+b = False
+c = a and b
+
+print(c)
+
+
+
