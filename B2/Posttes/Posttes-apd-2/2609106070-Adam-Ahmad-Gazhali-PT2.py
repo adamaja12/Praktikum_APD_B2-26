7c88b573349c6bcc53a2add5f8ed8200b8ea071e
adamaja12 <ghazaliadam01@gmail.com>
2026-09-21 17:46:50 +0800

    Update 2609106070-Adam-Ahmad-Gazhali-PT2.py

--- B2/Posttes/Posttes-apd-2/2609106070-Adam-Ahmad-Gazhali-PT2.py
+++ B2/Posttes/Posttes-apd-2/2609106070-Adam-Ahmad-Gazhali-PT2.py
@@ -1,17 +1,1 @@
-skincare_1 = int(35000)
-skincare_2 = int(42000)
-skincare_3 = int(50000)
-skincare_4 = int(55000)
-skincare_5 = int(68000)
-skincare_6 = int(70000)
 
-total = skincare_1 + skincare_2 + skincare_3 + skincare_4 + skincare_5 + skincare_6
-total_pajak = total + int(12000)
-rata_rata = total_ajak / 6
-
-nim = int("070")
-
-bolean = nim < rata_rata
-
-
-print (total_pajak)
